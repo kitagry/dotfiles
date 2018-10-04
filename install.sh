5c675593b52365f23589159cd875a1c213c3c56c
kitagawa <kitadrum50@gmail.com>
2018-10-04 15:01:31 +0900

    Fix install.sh

--- install.sh
+++ install.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
 
+set -eu
+
+DOT_DIRECTORY="${HOME}/dotfiles"
+DOT_CONFIG_DIRECTORY=".config"
+
 for f in .??*
 do
     [[ "$f" == ".git" ]] && continue
     [[ "$f" == ".gitignore" ]] && continue
+    [[ "$f" == ".config" ]] && continue
     [[ "$f" == ".DS_Store" ]] && continue
 
-    ln -s $HOME/dotfiles/$f $HOME/$f
+    ln -snfv ${DOT_DIRECTORY}/$f $HOME/$f
+done
+
+cd ./${DOT_CONFIG_DIRECTORY}
+for file in `\find . -maxdepth 4 -type f`; do
+    ln -snfv ${DOT_DIRECTORY}/${DOT_CONFIG_DIRECTORY}/${file:2} ${HOME}/${DOT_CONFIG_DIRECTORY}/${file:2}
 done
