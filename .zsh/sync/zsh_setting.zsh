2461c5609c105b13ce1310566efd54d2270c8bbf
Ryo Kitagawa <kitadrum50@gmail.com>
2023-02-17 17:11:43 +0900

    Do not save unnecessary zsh history

--- .zsh/sync/zsh_setting.zsh
+++ .zsh/sync/zsh_setting.zsh
@@ -23,3 +23,9 @@ export CLICOLOR=1
 export LSCOLORS=exfxcxdxbxegedabagacad
 export LS_COLORS='di=34:ln=35:so=32:pi=33:ex=31:bd=46;34:cd=43;34:su=41;30:sg=46;30:tw=42;30:ow=43;30'
 zstyle ':completion:*' list-colors 'di=34' 'ln=35' 'so=32' 'ex=31' 'bd=46;34' 'cd=43;34'
+
+# 不要なヒストリーは保存しないようにする
+zshaddhistory() {
+    local line="${1%%$'\n'}"
+    [[ ! "$line" =~ "^(cd|la|ll|ls|rm|rmdir|less)($| )" ]]
+}
