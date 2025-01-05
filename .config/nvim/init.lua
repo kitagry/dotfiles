d14296cf272d4f4f3c134d4d03a5bf0e137a4e18
Ryo Kitagawa <kitadrum50@gmail.com>
2025-01-05 21:58:20 +0900

    chore: add avante.nvim

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1191,7 +1191,50 @@ require("kitagry.lazy").setup({
       require("diffview").setup()
     end
   },
-  {"lambdalisue/vim-suda"}
+  {"lambdalisue/vim-suda"},
+  { "yetone/avante.nvim",
+    event = "VeryLazy",
+    build = "make",
+    dependencies = {
+      "stevearc/dressing.nvim",
+      "nvim-lua/plenary.nvim",
+      "MunifTanjim/nui.nvim",
+      --- The below dependencies are optional,
+      "hrsh7th/nvim-cmp", -- autocompletion for avante commands and mentions
+      "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
+      "zbirenbaum/copilot.lua", -- for providers='copilot'
+      {
+        -- support for image pasting
+        "HakonHarnes/img-clip.nvim",
+        event = "VeryLazy",
+        opts = {
+          -- recommended settings
+          default = {
+            embed_image_as_base64 = false,
+            prompt_for_file_name = false,
+            drag_and_drop = {
+              insert_mode = true,
+            },
+            -- required for Windows users
+            use_absolute_path = true,
+          },
+        },
+      },
+      {
+        -- Make sure to set this up properly if you have lazy=true
+        'MeanderingProgrammer/render-markdown.nvim',
+        opts = {
+          file_types = { "markdown", "Avante" },
+        },
+        ft = { "markdown", "Avante" },
+      },
+    },
+    config = function()
+      require("avante").setup({
+          provider = "copilot",
+      })
+    end
+  }
 })
 
 local local_path = vim.env.HOME .. '/.config/nvim/init.local.lua'
